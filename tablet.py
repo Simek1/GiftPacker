0fa4c2eaff816ed9c0e20fe41513b0feb13373ba
Simek1 <t.borowiak98@gmail.com>
2023-12-24 03:35:27 +0100

    Turning flashlight and flipping tablet

--- tablet.py
+++ tablet.py
@@ -16,13 +16,16 @@ class Tablet(QWidget):
 		self.tableton=QPixmap("imgs/tableton.png")
 		self.tabletcam=QPixmap("imgs/tabletcam.png")
 		self.tabletquiz=QPixmap("imgs/tabletquiz.png")
+		self.tabletback=QPixmap("imgs/tabletback.png")
 
-		imgs=[self.tableton, self.tabletoff, self.tabletcam, self.tabletquiz]
+		imgs=[self.tableton, self.tabletoff, self.tabletcam, self.tabletquiz, self.tabletback]
 
 		desktop=QApplication.desktop()
 		screen_geometry=desktop.screenGeometry()
 		self.screen_width=screen_geometry.width()
 		screen_height=screen_geometry.height()
+
+		self.scalar=1
 
 		if self.screen_width<1920:
 			self.scalar=self.screen_width/1920
@@ -40,6 +43,9 @@ class Tablet(QWidget):
 		self.light_on=False
 		self.quiz_on=False
 		self.safe_on=False
+		self.flipped=False
+
+		self.light=Light(50+self.pos().x(), self.pos().y()+55)
 
 	def contextMenuEvent(self, event):
 		menu = QMenu(self)
@@ -65,9 +71,30 @@ class Tablet(QWidget):
 			elif 51<=event.x()<=130 and 48<=event.y()<=128 and self.tablet_on:
 				self.label.setPixmap(self.tabletcam)
 				self.cam_on=True
+			elif 165<=event.x()<=245 and 48<=event.y()<=128 and self.tablet_on:
+				if self.light_on:
+					self.light_on=False
+					self.light.hide()
+				else:
+					self.light_on=True
+					self.light.show()
 			elif 279<=event.x()<=357 and 48<=event.y()<=128 and self.tablet_on:
 				self.label.setPixmap(self.tabletquiz)
 				self.quiz_on=True
+			elif 745<=event.x()<=785 and 0<=event.y()<=40 and self.flipped==False:
+				self.label.setPixmap(self.tabletback)
+				self.flipped=True
+				self.tablet_on=False
+				self.quiz_on=False
+				self.cam_on=False
+				self.safe_on=False
+				if self.light_on:
+					self.light.tablet_flip()
+			elif 0<=event.x()<=41 and 0<=event.y()<=40 and self.flipped:
+				self.label.setPixmap(self.tabletoff)
+				self.flipped=False
+				if self.light_on:
+					self.light.tablet_flip()
 			elif 496<=event.x()<=578 and 233<=event.y()<=264 and self.quiz_on:
 				self.label.setPixmap(self.tableton)
 				self.quiz_on=False
@@ -76,7 +103,39 @@ class Tablet(QWidget):
 
 	def mouseMoveEvent(self, event):
 		if event.buttons()==Qt.LeftButton:
+			old_pos=self.pos()
 			self.move(event.globalPos() - self.offset)
+			new_pos=self.pos()
+			self.light.move(self.light.pos()+new_pos-old_pos)
+
+class Light(QWidget):
+	def __init__(self, pos_x, pos_y):
+		super().__init__()
+		self.initUI(pos_x, pos_y)
+
+	def initUI(self, pos_x, pos_y):
+		self.setWindowFlags(Qt.FramelessWindowHint | Qt.Tool)
+		self.setAttribute(Qt.WA_TranslucentBackground)
+
+		self.light=QPixmap("imgs/light.png")
+		self.label=QLabel(self)
+		self.label.setPixmap(self.light)
+		self.label.setGeometry(0, 0, self.light.width(), self.light.height())
+		self.setGeometry(pos_x-int(self.light.width()/2), pos_y-int(self.light.height()/2), self.light.width(), self.light.height())
+
+		self.tablet_flipped=False
+	
+	def tablet_flip(self):
+		if self.tablet_flipped:
+			self.setWindowFlags(Qt.FramelessWindowHint | Qt.Tool)
+			self.move(self.pos().x()-685, self.pos().y())
+			self.show()
+			self.tablet_flipped=False
+		else:
+			self.setWindowFlags(Qt.FramelessWindowHint | Qt.Tool | Qt.WindowStaysOnTopHint)
+			self.show()
+			self.move(self.pos().x()+685, self.pos().y())
+			self.tablet_flipped=True
 
 if __name__ == '__main__':
 	app=QApplication(sys.argv)
