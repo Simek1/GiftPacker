e5deb2773d82543c3d661a5a27e80a3aabfeb888
Simek1 <t.borowiak98@gmail.com>
2024-01-24 19:41:20 +0100

    Loader works properly

--- loader.py
+++ loader.py
@@ -1,10 +1,8 @@
 import os
 import sys
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from base64 import urlsafe_b64encode, urlsafe_b64decode
+from base64 import urlsafe_b64decode
 
 class Settings(object):
     def __init__(self):
@@ -17,14 +15,36 @@ class Settings(object):
             decryptor=cipher.decryptor()
             settings=decryptor.update(urlsafe_b64decode(ciphered_settings)) + decryptor.finalize()
             settings=settings.decode(errors="ignore")
-            print(settings)
-        settings=settings.splitlines
+        settings=settings.splitlines()
         if len(settings)>8:
-            pass
+            try:
+                self.rew_msg=settings[0]
+                self.game_code=settings[1]
+                self.mys1_msg=settings[2]
+                self.mys1_code=settings[3]
+                self.mys2_msg=settings[4]
+                self.mys2_code=settings[5]
+                self.safe_code=settings[6]
+                self.questions_num=int(settings[7])
+                self.questions=[]
+                self.answers=[]
+                for i in range(8,8+self.questions_num*5, 5):
+                    answers=[]
+                    self.questions.append(settings[i])
+                    answers.append(settings[i+1])
+                    answers.append(settings[i+2])
+                    answers.append(settings[i+3])
+                    answers.append(settings[i+4])
+                    self.answers.append(answers)
+            except:
+                print("Corupted settings file")
+                return 0
         else:
-            return 0
             print("Corrupted settings file")
+            return 0
 
 if __name__ == "__main__":
-    Settings()
+    setts=Settings()
+    for ar in dir(setts):
+        print(ar, getattr(setts, ar))
     sys.exit()
