5bab02ae522ca4fb87a5a7c196106073117d9306
Simek1 <t.borowiak98@gmail.com>
2024-01-25 19:43:30 +0100

    Setup window appears if there is no settings file

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import sys
+import os
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QAction, QMenu
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt, QTimer
 from box import *
 from tablet import *
+from settings import SettingsWindow
 
 def openSecondWindow():
 	window.close()
@@ -11,9 +13,17 @@ def openSecondWindow():
 	window2.rolled_sheet.show()
 
 if __name__ == '__main__':
-	app=QApplication(sys.argv)
-	window=Box()
-	window.show()	
-	window2=Tablet()
-	window.destroyed.connect(openSecondWindow)
+	if os.path.exists("conf"):
+		try:
+			app=QApplication(sys.argv)
+			window=Box()
+			window.show()	
+			window2=Tablet()
+			window.destroyed.connect(openSecondWindow)
+		except:
+			pass
+	else:
+		app = QApplication(sys.argv)
+		setup_window=SettingsWindow()
+		setup_window.show()
 	sys.exit(app.exec_())
